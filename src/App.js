4060fc3f8fda0d217f9f9939e0ae6ca133169741
anu radha <anu.sivasubramanian@gmail.com>
2020-06-10 16:14:39 +0200

    added webkit for animation and transition

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import About from "./components/About";
 import Contact from "./components/Contact";
